901520e290b946fa126825648e3c3d7866307bff
Joran Elias <joran.elias@umontana.edu>
2015-04-28 08:28:51 -0600

    use cairo when deploying

--- server.R
+++ server.R
@@ -2,6 +2,8 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
+#library(Cairo)
+#options(shiny.usecairo = TRUE)
 source("helpers.R")
 source("load_data.R")
 
@@ -13,13 +15,17 @@ shinyServer(function(input, output) {
                    events = input$events)
   })
   
+  observe({elite_data <- elite()})
+  
   ath <- reactive({
     ath_data(nms = input$nameInput,by_tech = input$by_tech)
   })
+  observe({ath_data <- ath()})
   
+  #Output
   output$comp_grp_n <- renderText({
-    paste("Comparison group currently contains <b>",
-          elite()$n_gender[1],"</b>men and <b>",elite()$n_gender[2],"</b>women.")
+      paste("Comparison group currently contains <b>",
+            elite()$n_gender[1],"</b>men and <b>",elite()$n_gender[2],"</b>women.")
   })
   
   output$plot1 <- renderPlot({
