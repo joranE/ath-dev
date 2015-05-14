435c84ce549d7daeae7518f6f3a67b577cf1bd41
Joran Elias <joran.elias@gmail.com>
2015-05-14 10:36:00 -0400

    update

--- ui.R
+++ ui.R
@@ -1,44 +1,45 @@
 library(shiny)
+library(shinydashboard)
 source("load_names.R")
 
-shinyUI(fluidPage(
+# Application title
+header <- dashboardHeader(title = "Statistical Skier")
 
-  # Application title
-  titlePanel("Athlete Development"),
+# Sidebar with a slider input for number of bins
+sidebar <- dashboardSidebar(
+  h4("Define comparison group:"),
+  numericInput("top","Finished in the top:",3),
+  numericInput("times","At least this many times:",5),
+  checkboxGroupInput(inputId = "events",
+                     label = "In the following events:",
+                     choices = c("World Cup"         = "WC",
+                                 "Olympics"          = "OWG",
+                                 "World Ski Champs"  = "WSC",
+                                 "Tour de Ski"       = "TDS"),
+                     selected = c("WC","OWG","WSC","TDS")),
+  hr(),
+  selectizeInput(inputId = "nameInput",
+                 label = "Choose an athlete:",
+                 choices = c("Start typing..." = "",NAMES),
+                 selected = NULL,
+                 multiple = TRUE,
+                 options = list(maxItems = 4)),
+  checkboxInput(inputId = "by_tech",
+                label = "Plot by technique:",
+                value = FALSE),
+  submitButton("Plot Development Comparison"),
+  hr(),
+  htmlOutput("comp_grp_n")
+)
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      h4("Define comparison group:"),
-      numericInput("top","Finished in the top:",3),
-      numericInput("times","At least this many times:",5),
-      checkboxGroupInput(inputId = "events",
-                    label = "In the following events:",
-                    choices = c("World Cup"         = "WC",
-                                "Olympics"          = "OWG",
-                                "World Ski Champs"  = "WSC",
-                                "Tour de Ski"       = "TDS"),
-                    selected = c("WC","OWG","WSC","TDS")),
-      hr(),
-      htmlOutput("comp_grp_n"),
-      hr(),
-      selectizeInput(inputId = "nameInput",
-                  label = "Choose an athlete:",
-                  choices = c("Start typing..." = "",NAMES),
-                  selected = NULL,
-                  multiple = TRUE,
-                  options = list(maxItems = 4)),
-      checkboxInput(inputId = "by_tech",
-                    label = "Plot by technique:",
-                    value = FALSE),
-      submitButton("Plot Development Comparison")
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("plot1"),
-      plotOutput("plot2"),
-      plotOutput("plot3")
-    )
+# Show a plot of the generated distribution
+body <- dashboardBody(
+  fluidRow(
+    plotOutput("plot1"),
+    plotOutput("plot2"),
+    plotOutput("plot3")
   )
-))
+)
+
+
+dashboardPage(header,sidebar,body)
